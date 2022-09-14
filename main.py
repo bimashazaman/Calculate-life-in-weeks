24ba3d673cedb045f5c252d0b2517a3313e6103e
bimashazaman <bimashazaman1234@gmail.com>
2022-09-14 20:01:58 +0600

    Maths

---
+++ main.py
@@ -0,0 +1,14 @@
+from calendar import month
+
+
+age = input("What is your current age?")
+
+age_as_int = int(age)
+
+years_left = 90 - age_as_int
+days_left = years_left * 365 
+weeks_left = years_left * 52
+months_left = years_left * 12
+
+
+
